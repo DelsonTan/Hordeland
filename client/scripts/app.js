5839b4dc1dc6efb5a2e85064020b2e4f2d43b635
Delson Tan <delsonwtan@gmail.com>
2018-08-24 07:35:37 +0000

    Implement chat system for debugging

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,9 +1,12 @@
 $(document).ready(function () {
     // ------------------------------------------------ Render Logic ------------------------------------------------
-    var canvas = $('#ctx')[0]
-    canvas.width = 500
-    canvas.height = 500
-    var ctx = canvas.getContext("2d")
+    // Canvas
+    var canvas = $('#ctx')
+    // Allow canvas to be focused for event listening
+    canvas.attr('tabindex', 0)
+    canvas[0].width = 500
+    canvas[0].height = 500
+    var ctx = canvas[0].getContext("2d")
     ctx.font = '30px Arial'
     var socket = io()
 
@@ -13,11 +16,10 @@ $(document).ready(function () {
             renderPlayers(data.players[i].number, data.players[i].x, data.players[i].y)
         }
         for (var i = 0; i < data.projectiles.length; i++) {
-            console.log(data.projectiles)
             renderProjectile(data.projectiles[i].x - 5, data.projectiles[i].y - 5)
         }
     })
-    
+
     function renderPlayers(val, xpos, ypos) {
         ctx.strokeRect(xpos - 5, ypos - 25, 30, 30)
         ctx.fillText(val, xpos, ypos)
@@ -26,30 +28,75 @@ $(document).ready(function () {
     function renderProjectile(xpos, ypos) {
         ctx.fillRect(xpos, ypos, 10, 5)
     }
+    // Chat
+    var chatText = $('#chat-text')
+    var chatInput = $('#chat-input')//[0]
+    chatInput.attr('tabindex', 0)
+    var chatForm = $('#chat-form')//[0]
+
+    
+
     // ------------------------------------------------ Event Handlers ------------------------------------------------
-    $(document).on("keydown", function(event) {
+    // Window
+    $(window).focus(function (event) {
+
+    })
+    // Canvas
+    canvas.attr('tabindex', 0)
+    canvas.focus()
+    canvas.on("keydown", function (event) {
         // WASD keys
         if (event.which === 65) {
-            socket.emit('keyPress', {inputId: 'left', state: true})
+            socket.emit('keyPress', { inputId: 'left', state: true })
         } else if (event.which === 68) {
-            socket.emit('keyPress', {inputId: 'right', state: true})
+            socket.emit('keyPress', { inputId: 'right', state: true })
         } else if (event.which === 87) {
-            socket.emit('keyPress', {inputId: 'up', state: true})
+            socket.emit('keyPress', { inputId: 'up', state: true })
         } else if (event.which === 83) {
-            socket.emit('keyPress', {inputId: 'down', state: true})
-        } 
+            socket.emit('keyPress', { inputId: 'down', state: true })
+        } else if (event.which === 13) {
+            // Force player to stop moving
+            socket.emit('keyPress', { inputId: 'left', state: false })
+            socket.emit('keyPress', { inputId: 'right', state: false })
+            socket.emit('keyPress', { inputId: 'up', state: false })
+            socket.emit('keyPress', { inputId: 'down', state: false })
+            // Switch focus to chat input
+            event.preventDefault()
+            canvas.blur()
+            chatInput.focus()
+        }
     })
 
-    $(document).on("keyup", function(event) {
+    canvas.on("keyup", function (event) {
         // WASD keys
         if (event.which === 65) {
-            socket.emit('keyPress', {inputId: 'left', state: false})
+            socket.emit('keyPress', { inputId: 'left', state: false })
         } else if (event.which === 68) {
-            socket.emit('keyPress', {inputId: 'right', state: false})
+            socket.emit('keyPress', { inputId: 'right', state: false })
         } else if (event.which === 87) {
-            socket.emit('keyPress', {inputId: 'up', state: false})
+            socket.emit('keyPress', { inputId: 'up', state: false })
         } else if (event.which === 83) {
-            socket.emit('keyPress', {inputId: 'down', state: false})
-        } 
+            socket.emit('keyPress', { inputId: 'down', state: false })
+        }
     })
+    // Chat
+    chatForm.submit(function (event) {
+        event.preventDefault()
+        if (chatInput.val()[0] === '/') {
+            socket.emit('evalMessage', { text: chatInput.val().slice(1) })
+        } else {
+            socket.emit('sendMessage', { text: chatInput.val() })
+        }
+        chatInput.val("")
+        chatInput.blur()
+        canvas.focus()
+    })
+
+    socket.on('addToChat', function (data) {
+        $("<div>").text(data).appendTo(chatText)
+    })
+    socket.on('evalAnswer', function (data) {
+        console.log(data)
+    })
+    // TODO: make chat scroll to bottom when new messages arrive
 })
--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const PORT = 3000
 
 const Player = require('./server/player.js')
 const Projectile = require('./server/projectile.js')
+// IMPORTANT: SET TO FALSE IN PRODUCTION
+const DEBUG = true
 
 let SOCKET_LIST = {}
 
@@ -15,6 +17,26 @@ io.sockets.on('connection', (socket) => {
     socket.id = Math.random()
     SOCKET_LIST[socket.id] = socket
     Player.onConnect(socket)
+
+    socket.on('evalMessage', (data) => {
+        if (!DEBUG) {
+            return
+        }
+        try {
+            var res = eval(data.text)
+            socket.emit('evalAnswer', res)
+        } catch(error) {
+            socket.emit('evalAnswer', `Error, invalid input: /${data.text}`)
+        }
+
+    })
+
+    socket.on('sendMessage', (data) => {
+        const playerName = (socket.id).toString().slice(2, 7)
+        for (let i in SOCKET_LIST) {
+            SOCKET_LIST[i].emit('addToChat', `${playerName}: ${data.text}`)
+        }
+    })
 
     socket.on('disconnect', () => {
         delete SOCKET_LIST[socket.id]
