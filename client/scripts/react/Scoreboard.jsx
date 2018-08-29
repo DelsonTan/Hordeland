1b610aa2b94b1e7e8a2383a19bd7d08a74967def
Rafael <rafaelmarianorocha@gmail.com>
2018-08-29 22:55:50 +0000

    wraps app for heroku deployment

--- client/scripts/react/Scoreboard.jsx
+++ client/scripts/react/Scoreboard.jsx
@@ -2,21 +2,9 @@ import React, { Component } from 'react'
 
 class Scoreboard extends Component {
 
-    // componentDidUpdate(prevProps) {
-    //     console.log("Scoreboard update:", this.state.players)
-        // if (prevProps.players.length === 0) {
-        //     console.log("Scoreboard: ", this.props.players)
-        // }
-        //     const newPlayers = this.state.players
-        //     newPlayers.push(JSON.parse(data))
-        //     console.log(newPlayers)
-        //     this.setState({players: newPlayers})
-        // }) 
-    // }
-
     render() {
         let playerTableRows = null
-        
+
         if (this.props.players.length > 0) {
             playerTableRows = this.props.players.map((player) => {
             return (
