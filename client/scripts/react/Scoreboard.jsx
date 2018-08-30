eab664cf7479d9e81ab024cb17b2e2dd1e714e06
Delson Tan <delsonwtan@gmail.com>
2018-08-30 01:00:02 +0000

    Remove commented out code

--- client/scripts/react/Scoreboard.jsx
+++ client/scripts/react/Scoreboard.jsx
@@ -2,18 +2,6 @@ import React, { Component } from 'react'
 
 class Scoreboard extends Component {
 
-    // componentDidUpdate(prevProps) {
-    //     console.log("Scoreboard update:", this.state.players)
-        // if (prevProps.players.length === 0) {
-        //     console.log("Scoreboard: ", this.props.players)
-        // }
-        //     const newPlayers = this.state.players
-        //     newPlayers.push(JSON.parse(data))
-        //     console.log(newPlayers)
-        //     this.setState({players: newPlayers})
-        // }) 
-    // }
-
     render() {
         let playerTableRows = null
         
